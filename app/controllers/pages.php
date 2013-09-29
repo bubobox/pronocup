fbf8cb30518a10e671be6146f9612c580ee60b64
Kristof Verbraeken <kristof.verbraeken@gmail.com>
2013-09-29 14:31:10 +0200

    Added methods for FB Connect

--- app/controllers/pages.php
+++ app/controllers/pages.php
@@ -9,17 +9,26 @@ class Pages extends XController {
 		$this->_data['predictions'] = $this->Prediction_model->all();
 		$this->_data['rankings'] = $this->Ranking_model->all();
 		$this->_data['scores'] = $this->Score_model->all();
-		$this->_data['users'] = $this->User_model->all();		
+		$this->_data['users'] = $this->User_model->all();
 		$this->render('pages/index');
+	}
+
+	public function create() {
+		$data['username']=$this->input->post('username');
+		$data['email']= $this->input->post('email');
+		$data['name']=$this->input->post('name');
+		$data['fbid']=$this->input->post('fbid');
+		$this->User_model->create($data);
 	}
 
 	public function json() {
 		$this->output ->set_content_type('application/json');
 		$data = array(
-			'a' => 1,
-			'b' => 2,
-			'c' => 3,
+				'a' => 1,
+				'b' => 2,
+				'c' => 3,
 		);
 		echo json_encode($data);
 	}
+
 }
--- app/models/user_model.php
+++ app/models/user_model.php
@@ -17,10 +17,23 @@ class User_model extends CI_Model {
 
 	function get_by_fbid($fbid) {
 		$result = $this->db->get_where(User_model::TABLE, array(
-			'fbid' => $fbid,
+				'fbid' => $fbid,
 		))->result();
 		if(count($result) == 1) {
 			return $result[0];
 		}
+	}
+
+	function create($data) {
+		if(userExists($data)){
+			$this->db->insert(User_model::TABLE, $data);
+		}
+	}
+
+	function userExists($data) {
+		$query = $this->db->query("SELECT * FROM users WHERE fbid=?;",array($data->fbid));
+		if ($query->result()->num_rows() > 0) return true;
+		return false;
+
 	}
 }
