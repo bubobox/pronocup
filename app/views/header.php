f062879ea503e472b4d8f3848049fb34bb1d3426
Kristof Verbraeken <kristof.verbraeken@gmail.com>
2013-09-29 16:18:40 +0200

    Login menu

--- app/views/header.php
+++ app/views/header.php
@@ -28,6 +28,10 @@
         <p><pre><?php print_r($user); ?></pre></p>
         <?php
         	if($is_authenticated) {
-        ?><p><a href="<?php echo $base ?>index.php/pages/logout">Logout</a></p><?php
+?><ul>
+	<li><strong><?php echo escape($user->name) ?></strong></li>
+	<li><a href="<?php echo $base ?>index.php/predictions/index">Predictions</a></li>
+	<li><a href="<?php echo $base ?>index.php/pages/logout">Logout</a></li>
+</ul><?php
         	}
         ?>
