d9123e25439f35a048a82798b6ed6f548697038e
Acoustics <acoustics-developer@hotmail.com>
2013-09-29 18:58:55 +0200

    Assets

---
+++ www/assets/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "assets/styles"
+sass_dir = "assets/scss"
+images_dir = "assets/images"
+javascripts_dir = "assets/scripts"
+
+output_style = :nested
+environment = :development
+relative_assets = true
+line_comments = false
+color_output = false
+
+require "susy"
+require "compass"
